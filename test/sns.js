e2526213926e21871890419ccd0e343888d1f761
donatPeter <peterdonat.bme@gmail.com>
2018-07-10 09:15:59 +0200

    fix error handling, using promises on testing

--- test/sns.js
+++ test/sns.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 
 const amazon = require('../lib/amazon.js');
 
-describe('snsResponse', () => {
+describe('==snsResponse tests==', () => {
   describe('parseSNSResponse', () => {
     it('should callback with an error if the certificate is hosted on an invalid host', (done) => {
       const response = {
@@ -15,11 +15,13 @@ describe('snsResponse', () => {
         SigningCertURL: 'https://sns.us-east-1.notamazonaws.com/cert.pem',
         Signature: 'gibberish',
       };
-      amazon.parseSNSResponse(response, (err) => {
-        assert.ok(err);
-        assert.equal(err.name, 'invalid_certificate_domain');
-        done();
-      });
+      amazon.parseSNSResponse(response)
+        .then(res => new Error(res))
+        .catch((err) => {
+          assert.ok(err);
+          assert.equal(err.name, 'invalid_certificate_domain');
+          done();
+        });
     });
   });
 });
